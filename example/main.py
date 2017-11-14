cd4138447c846d1c5f2199c056c8979c4ff5f920
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2017-11-14 21:45:22 +0100

    Added stub for an example application.

---
+++ example/main.py
@@ -0,0 +1,8 @@
+#
+# System to monitor
+#  - position (via GPS)
+#  - temperature (via I2C sensors)
+#  - status of various switch contacts (via GPIOs)
+#
+# The data collected will be forwarded via HTTP and stored locally if the connection
+# is not available.
