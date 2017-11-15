3daf22f9c5112be273350ea71e35278f89553d05
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2017-11-15 13:19:28 +0100

    Added stub for DataLogger main class.

--- datalogger/data_logger_test.py
+++ datalogger/data_logger_test.py
@@ -1,7 +1,14 @@
 import unittest
 
+import data_logger
+
 class DataLoggerTestCase(unittest.TestCase):
-	pass
+
+	def setUp(self):
+		self.dl = data_logger.DataLogger()
+
+	def testFoo(self):
+		self.assertEqual(True, True)
 
 if __name__ == "__main__":
 	unittest.main()
