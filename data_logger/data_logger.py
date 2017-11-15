e31b3007fc460a238f83b821cfd4af996b0d74bc
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2017-11-15 16:54:49 +0100

    Made entries in data_inputs DataInput objects.

--- data_logger/data_logger.py
+++ data_logger/data_logger.py
@@ -1,7 +1,9 @@
+import data_input
+
 class DataLogger():
 
 	def __init__(self, config):
 		self.config = config
 		self.data_inputs = []
 		for di in self.config.data_inputs:
-			self.data_inputs.append("Foo")
+			self.data_inputs.append(data_input.DataInput("Foo"))
--- data_logger/data_logger_test.py
+++ data_logger/data_logger_test.py
@@ -2,6 +2,7 @@ import unittest
 
 import data_logger
 import config
+import data_input
 
 class DataLoggerTestCase(unittest.TestCase):
 
@@ -13,6 +14,15 @@ class DataLoggerTestCase(unittest.TestCase):
 
 	def testInit_dataInputsIsPopulatedAccordingToConfig(self):
 		self.assertEqual(len(self.expectedCfg.data_inputs), len(self.dl.data_inputs))
+
+	def testInit_dataInputsIsListOfDataInputObjects(self):
+		expectedType = data_input.DataInput
+		ofExpectedType = True
+		for obj in self.dl.data_inputs:
+			if not isinstance(obj, expectedType):
+				ofExpectedType = False
+				break
+		self.assertEqual(True, ofExpectedType)
 
 if __name__ == "__main__":
 	unittest.main()
