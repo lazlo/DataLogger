3864af17147c43ebc9067201e9a2d5038d81cbea
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2017-11-15 13:26:19 +0100

    Made DataLogger populate data_inputs attribute.

--- datalogger/data_logger.py
+++ datalogger/data_logger.py
@@ -1,2 +1,7 @@
 class DataLogger():
-	pass
+
+	def __init__(self, config):
+		self.config = config
+		self.data_inputs = []
+		for di in self.config.data_inputs:
+			self.data_inputs.append("Foo")
--- datalogger/data_logger_test.py
+++ datalogger/data_logger_test.py
@@ -1,14 +1,18 @@
 import unittest
 
 import data_logger
+import config
 
 class DataLoggerTestCase(unittest.TestCase):
 
 	def setUp(self):
-		self.dl = data_logger.DataLogger()
+		self.expectedCfgFile = "data_logger_test_cfg.json"
+		self.expectedCfg = config.Config()
+		self.expectedCfg.load_file(self.expectedCfgFile)
+		self.dl = data_logger.DataLogger(self.expectedCfg)
 
-	def testFoo(self):
-		self.assertEqual(True, True)
+	def testInit_populatesDataInputsAccordingToConfig(self):
+		self.assertEqual(len(self.expectedCfg.data_inputs), len(self.dl.data_inputs))
 
 if __name__ == "__main__":
 	unittest.main()
