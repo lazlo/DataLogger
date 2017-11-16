82577065e644e6e27914384d80b396a9c7dead6d
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2017-11-16 17:32:06 +0100

    Updated example.

--- example/main.py
+++ example/main.py
@@ -6,3 +6,14 @@
 #
 # The data collected will be forwarded via HTTP and stored locally if the connection
 # is not available.
+
+import data_logger
+
+def main():
+	cfg = data_logger.Config()
+	cfg.load_file("../data_logger/data_logger_test_cfg.json")
+	dl = data_logger.DataLogger(cfg)
+	dl.update()
+
+if __name__ == "__main__":
+	main()
