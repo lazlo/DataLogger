7833420e314a5d11deb87bfb72f0e8b11f93a31a
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2017-11-16 16:28:56 +0100

    Pass addres, user and password to server.

--- data_logger/data_server.py
+++ data_logger/data_server.py
@@ -1,2 +1,6 @@
 class DataServer():
-	pass
+
+	def __init__(self, addr, user, password):
+		self.addr = addr
+		self.user = user
+		self.password = password
--- data_logger/data_server_test.py
+++ data_logger/data_server_test.py
@@ -5,7 +5,16 @@ import data_server
 class DataServerTestCase(unittest.TestCase):
 
 	def setUp(self):
-		self.srv = data_server.DataServer()
+		self.expectedAddr = "http://localhost"
+		self.expectedUser = "eris"
+		self.expectedPassword = "fnord2342"
+		self.srv = data_server.DataServer(self.expectedAddr, self.expectedUser, self.expectedPassword)
 
-	def testFoo(self):
-		self.assertEqual(True, True)
+	def testInit_addrIsSet(self):
+		self.assertEqual(self.expectedAddr, self.srv.addr)
+
+	def testInit_userIsSet(self):
+		self.assertEqual(self.expectedUser, self.srv.user)
+
+	def testInit_passwordIsSet(self):
+		self.assertEqual(self.expectedPassword, self.srv.password)
