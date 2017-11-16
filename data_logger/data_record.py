de6d3972313c551f3b4bdb5d36f41ebd13f83330
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2017-11-16 17:50:44 +0100

    Added TODO to DataRecord.

--- data_logger/data_record.py
+++ data_logger/data_record.py
@@ -1,2 +1,12 @@
+# TODO Allow transformation of object to the following format
+# {
+#     "LoggerName": "SomeName",
+#     "Records": [
+#         {"Timestamp": "2017-04-28T00:00:00",
+#          "Measurements": [44.44, 55.55, 66.66],
+#         }
+#     ]
+# }
+
 class DataRecord():
 	pass
