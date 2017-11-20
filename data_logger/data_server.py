c551195f39c881b52c5aa0f444f819aceb09fd55
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2017-11-20 09:38:37 +0100

    Allow uploading to a specific URL.

--- data_logger/data_server.py
+++ data_logger/data_server.py
@@ -6,16 +6,16 @@ class DataServer():
 
 	def __init__(self, addr, user, password):
 		url = urlparse.urlparse(addr)
-
 		self.host = url.hostname
 		self.port = url.port
+		self.path = url.path
 		self.user = user
 		self.password = password
 		self.httpconn = httplib.HTTPConnection(self.host, self.port)
 
 	def upload(self, body):
 		method = "POST"
-		url = ""
+		url = self.path
 		body = urllib.urlencode(body)
 		headers = {"Content-type": "application/json;charset=utf-8", "Accept": "application/json"}
 		self.httpconn.request(method, url, body, headers)
--- data_logger/data_server_test.py
+++ data_logger/data_server_test.py
@@ -6,16 +6,19 @@ import urllib
 
 httpconn_request_called = False
 httpconn_request_arg_method = None
+httpconn_request_arg_url = None
 httpconn_request_arg_body = None
 httpconn_request_arg_headers = None
 
 def fake_httpconn_request(method, url, body=None, headers=None):
 	global httpconn_request_called
 	global httpconn_request_arg_method
+	global httpconn_request_arg_url
 	global httpconn_request_arg_body
 	global httpconn_request_arg_headers
 	httpconn_request_called = True
 	httpconn_request_arg_method = method
+	httpconn_request_arg_url = url
 	httpconn_request_arg_body = body
 	httpconn_request_arg_headers = headers
 
@@ -24,7 +27,8 @@ class DataServerTestCase(unittest.TestCase):
 	def setUp(self):
 		self.expectedHost = "192.168.0.1"
 		self.expectedPort = 8081
-		self.expectedAddr = "http://%s:%d/db/req/type/json/store" % (self.expectedHost, self.expectedPort)
+		self.expectedUrl = "/db/req/type/json/store"
+		self.expectedAddr = "http://%s:%d%s" % (self.expectedHost, self.expectedPort, self.expectedUrl)
 		self.expectedUser = "eris"
 		self.expectedPassword = "fnord2342"
 		self.requestMethod = "POST"
@@ -63,6 +67,13 @@ class DataServerTestCase(unittest.TestCase):
 		self.srv.httpconn.request = fake_httpconn_request
 		self.srv.upload(self.requestBody)
 		self.assertEqual(self.requestMethod, httpconn_request_arg_method)
+
+	def testUpload_callsHttpConnRequestWithSecondArgumentUrl(self):
+		global httpconn_request_arg_url
+		httpconn_request_arg_url = None
+		self.srv.httpconn.request = fake_httpconn_request
+		self.srv.upload(self.requestBody)
+		self.assertEqual(self.expectedUrl, httpconn_request_arg_url)
 
 	def testUpload_callsHttpConnRequestWithThirdArgumentBodyUrlEncoded(self):
 		global httpconn_request_arg_body
