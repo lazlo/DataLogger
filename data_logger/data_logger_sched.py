bc49fe011414e9d321491f77ba879175eb542ddf
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2017-11-21 17:26:14 +0100

    Sched: Implement simple update() and dispatch().

--- data_logger/data_logger_sched.py
+++ data_logger/data_logger_sched.py
@@ -10,3 +10,18 @@ class DataLoggerScheduler():
 		task.update({"delay_sec": delay_sec})
 		task.update({"do_run": False})
 		self.tasks.append(task)
+
+	def update(self):
+		for t in self.tasks:
+			if t["delay_sec"] > 0:
+				t["delay_sec"] -= 1
+			if t["delay_sec"] == 0:
+				t["do_run"] = True
+
+	def dispatch(self):
+		for t in self.tasks:
+			if not t["do_run"]:
+				continue
+			t["fp"]()
+			t["do_run"] = False
+			t["delay_sec"] = t["period_sec"]
--- data_logger/data_logger_sched_test.py
+++ data_logger/data_logger_sched_test.py
@@ -2,13 +2,16 @@ import unittest
 
 import data_logger_sched
 
-def testTask1(self):
+testTask1_called = False
+
+def testTask1():
+	global testTask1_called
+	testTask1_called = True
+
+def testTask2():
 	return
 
-def testTask2(self):
-	return
-
-def testTask3(self):
+def testTask3():
 	return
 
 class DataLoggerSchedTestCase(unittest.TestCase):
@@ -50,3 +53,52 @@ class DataLoggerSchedTestCase(unittest.TestCase):
 	def testAdd_insertsTaskWithDoRunSetToFalse(self):
 		self.sch.add(testTask1, 90, 0)
 		self.assertEqual(False, self.sch.tasks[0]["do_run"])
+
+	#
+	# update()
+	#
+
+	def testUpdate_decrementsTaskDelaySecIfNotZero(self):
+		self.sch.add(testTask3, 0, 10)
+		self.sch.update()
+		self.sch.update()
+		self.sch.update()
+		self.assertEqual(7, self.sch.tasks[0]["delay_sec"])
+
+	def testUpdate_decrementsTaskDelaySecOnlyWhenGreaterZero(self):
+		self.sch.add(testTask2, 0, 2)
+		self.sch.update()
+		self.sch.update()
+		self.sch.update()
+		self.assertEqual(0, self.sch.tasks[0]["delay_sec"])
+
+	def testUpdate_setsDoRunTrueWhenDelaySecBecomesZero(self):
+		self.sch.add(testTask2, 0, 2)
+		self.sch.update()
+		self.sch.update()
+		self.assertEqual(True, self.sch.tasks[0]["do_run"])
+
+	#
+	# dispatch()
+	#
+
+	def testDispatch_executesTaskWhereDoRunIsTrue(self):
+		global testTask1_called
+		testTask1_called = False
+		self.sch.add(testTask1, 0, 0)
+		self.sch.update()
+		self.sch.dispatch()
+		self.assertEqual(True, testTask1_called)
+
+	def testDispatch_setsDoRunFalseAfterTaskWasExecuted(self):
+		self.sch.add(testTask1, 0, 0)
+		self.sch.update()
+		self.sch.dispatch()
+		self.assertEqual(False, self.sch.tasks[0]["do_run"])
+
+	def testDispatch_reloadsDelayWithPeriodAfterTaskWasExecuted(self):
+		period_sec = 10
+		self.sch.add(testTask1, period_sec, 0)
+		self.sch.update()
+		self.sch.dispatch()
+		self.assertEqual(period_sec, self.sch.tasks[0]["delay_sec"])
