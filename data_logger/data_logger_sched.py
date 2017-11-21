f7d4160f2d149181b98d34c7d51141a244a13053
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2017-11-21 17:05:52 +0100

    Sched: Have add() to add new tasks.

--- data_logger/data_logger_sched.py
+++ data_logger/data_logger_sched.py
@@ -1,3 +1,12 @@
 class DataLoggerScheduler():
 
-	pass
+	def __init__(self):
+		self.tasks = []
+
+	def add(self, fp, period_sec, delay_sec):
+		task = {}
+		task.update({"fp": fp})
+		task.update({"period_sec": period_sec})
+		task.update({"delay_sec": delay_sec})
+		task.update({"do_run": False})
+		self.tasks.append(task)
--- data_logger/data_logger_sched_test.py
+++ data_logger/data_logger_sched_test.py
@@ -2,10 +2,51 @@ import unittest
 
 import data_logger_sched
 
+def testTask1(self):
+	return
+
+def testTask2(self):
+	return
+
+def testTask3(self):
+	return
+
 class DataLoggerSchedTestCase(unittest.TestCase):
 
 	def setUp(self):
 		self.sch = data_logger_sched.DataLoggerScheduler()
 
-	def testFoo(self):
-		self.assertEqual(True, True)
+	#
+	# __init__()
+	#
+
+	def testInit_taskListIsEmpty(self):
+		self.assertEqual(0, len(self.sch.tasks))
+
+	#
+	# add()
+	#
+
+	def testAdd_insertsTaskIntoTaskList(self):
+		self.sch.add(testTask1, 1, 0)
+		self.sch.add(testTask2, 1, 0)
+		self.sch.add(testTask3, 1, 0)
+		self.assertEqual(3, len(self.sch.tasks))
+
+	def testAdd_insertsTaskIntoTaskListWithFirstArgumentAsFunctionPointer(self):
+		self.sch.add(testTask1, 1, 0)
+		self.assertEqual(testTask1, self.sch.tasks[0]["fp"])
+
+	def testAdd_insertsTaskIntoTaskListWithSecondArgumentPeriodInSeconds(self):
+		period_sec = 20
+		self.sch.add(testTask1, period_sec, 0)
+		self.assertEqual(period_sec, self.sch.tasks[0]["period_sec"])
+
+	def testAdd_insertsTaskIntoTaskListWithThirdArgumentDelayInSeconds(self):
+		delay_sec = 10
+		self.sch.add(testTask1, 15, delay_sec)
+		self.assertEqual(delay_sec, self.sch.tasks[0]["delay_sec"])
+
+	def testAdd_insertsTaskWithDoRunSetToFalse(self):
+		self.sch.add(testTask1, 90, 0)
+		self.assertEqual(False, self.sch.tasks[0]["do_run"])
