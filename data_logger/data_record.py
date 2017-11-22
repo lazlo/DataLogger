d1e33709dd4096b3afd6716aea945554823e5eff
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2017-11-22 17:31:11 +0100

    DataRecord: Removed code that was created with a wrong understanding of how objects are converted to JSON.

--- data_logger/data_record.py
+++ data_logger/data_record.py
@@ -27,6 +27,5 @@ class DataRecord():
 
 	@staticmethod
 	def from_json_string(input_str):
-		input_str = input_str.replace("'", "\"")
 		data = json.loads(input_str)
 		return DataRecord(data["timestamp"], data["measurements"])
--- data_logger/data_record_test.py
+++ data_logger/data_record_test.py
@@ -36,31 +36,5 @@ class DataRecordTestCase(unittest.TestCase):
 
 	def testFromJSONString(self):
 		expected = data_record.DataRecord(self.expectedTimestamp, self.expectedMeasurements)
-		# NOTE python JSON parser seems to be able to only handle double qouted strings
-		input_str = str(expected.__dict__).replace("'", "\"")
+		input_str = json.dumps(expected.__dict__)
 		self.assertEqual(expected.__dict__, data_record.DataRecord.from_json_string(input_str).__dict__)
-
-	def testFromJSONString_replacesSingleWithDoubleQuotesInInputString(self):
-		global json_loads_arg
-		global json_loads_value
-
-		dr = data_record.DataRecord(self.expectedTimestamp, self.expectedMeasurements)
-
-		json_loads_arg = None
-		json_loads_value = dr.__dict__
-
-		# save original function pointer
-		original_json_loads = json.loads
-		# overwrite function pointer
-		json.loads = fake_json_loads
-
-		# prepare the string we expected to be passed to json.loads()
-		expected_json_loads_arg = str(dr.__dict__).replace("'", "\"")
-		# create the string that we will pass to DataRecord.from_json_string (that contains single qoutes)
-		single_quoted_string = str(dr.__dict__)
-		try:
-			data_record.DataRecord.from_json_string(single_quoted_string)
-			self.assertEqual(expected_json_loads_arg, json_loads_arg)
-		finally:
-			# restore original function pointer
-			json.loads = original_json_loads
