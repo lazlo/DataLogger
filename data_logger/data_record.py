30f542ba35631993e38135dee89751de67bb2cfd
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2017-11-27 14:13:20 +0100

    DataRecord: Removed TODO comment.

--- data_logger/data_record.py
+++ data_logger/data_record.py
@@ -1,12 +1,3 @@
-# TODO Allow transformation of object to the following format
-# {
-#     "LoggerName": "SomeName",
-#     "Records": [
-#         {"Timestamp": "2017-04-28T00:00:00",
-#          "Measurements": [44.44, 55.55, 66.66],
-#         }
-#     ]
-# }
 import time
 import json
 
