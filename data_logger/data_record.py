b0eedb6ccade0d995dd28c7c08e519918aaa0fbd
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2017-11-27 14:12:07 +0100

    DataRecord: Have field for timestamp format.

--- data_logger/data_record.py
+++ data_logger/data_record.py
@@ -12,11 +12,13 @@ import json
 
 class DataRecord():
 
+	TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S"
+
 	def __init__(self, timestamp=None, measurements=None):
 		if timestamp:
 			self.timestamp = timestamp
 		else:
-			self.timestamp = time.strftime("%Y-%m-%dT%H:%M:%S")
+			self.timestamp = time.strftime(self.TIMESTAMP_FORMAT)
 		if measurements:
 			self.measurements = measurements
 		else:
