fee22e634a21511bf6eb76772f404c23a86b6254
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2017-12-06 17:36:02 +0100

    Temperature: Factor Fake sensor into extra class.

--- data_logger/data_input/temperature_test.py
+++ data_logger/data_input/temperature_test.py
@@ -3,22 +3,13 @@ import os
 
 import temperature
 
-class TemperatureTestCase(unittest.TestCase):
+class FakeTemperatureSensorFile():
 
-	def setUp(self):
-		self.expectedName = "Room 42"
-		self.expectedFile = "test-sensor-file.txt"
-		self.expectedArgs = {"path": self.expectedFile}
-		self.expectedLines = []
-		self.expectedLines.append("81 01 4b 01 7f ff 0c 10 9c : crc=9c YES\n")
-		self.expectedLines.append("81 01 4b 01 7f ff 0c 10 9c t=-24062\n")
+	LINES = ["81 01 4b 01 7f ff 0c 10 9c : crc=9c YES\n", "81 01 4b 01 7f ff 0c 10 9c t=-24062\n"]
 
-		self._init_fake_sensor_file(self.expectedFile, self.expectedLines)
-
-		self.t = temperature.Temperature(self.expectedName, self.expectedArgs)
-
-	def tearDown(self):
-		os.remove(self.expectedFile)
+	def __init__(self, path):
+		self.path = path
+		self._init_fake_sensor_file(self.path, self.LINES)
 
 	def _init_fake_sensor_file(self, path, lines):
 		f = open(path, "w")
@@ -26,12 +17,27 @@ class TemperatureTestCase(unittest.TestCase):
 			f.write(line)
 		f.close()
 
+	def remove(self):
+		os.remove(self.path)
+
+class TemperatureTestCase(unittest.TestCase):
+
+	def setUp(self):
+		self.expectedName = "Room 42"
+		self.expectedFile = "test-sensor-file.txt"
+		self.expectedArgs = {"path": self.expectedFile}
+		self.fs = FakeTemperatureSensorFile(self.expectedFile)
+		self.t = temperature.Temperature(self.expectedName, self.expectedArgs)
+
+	def tearDown(self):
+		self.fs.remove()
+
 	def testGetData_returnsFloat(self):
 		self.assertEqual(True, type(self.t.get_data()) is float)
 
 	def testGetData_readsLinesFromFileSpecifiedAsPath(self):
 		self.t.get_data()
-		self.assertEqual(self.expectedLines, self.t.lines)
+		self.assertEqual(FakeTemperatureSensorFile.LINES, self.t.lines)
 
 	def testGetData_parsesTemperatureInCelsiusAsFloatFromLines(self):
 		self.assertEqual(-24.062, self.t.get_data())
