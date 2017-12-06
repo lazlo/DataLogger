bf1790252ea7e66ef828d97965ed8c1ba5da4bda
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2017-12-06 17:15:35 +0100

    Temperature: Read lines from file on call of get_data().

--- data_logger/data_input/temperature.py
+++ data_logger/data_input/temperature.py
@@ -2,5 +2,19 @@ import sensor
 
 class Temperature(sensor.Sensor):
 
+	def __init__(self, name, args):
+		sensor.Sensor.__init__(self, name, args)
+		self.lines = []
+
+	def _read_file(self, path):
+		lines = []
+		f = open(path)
+		for line in f.readlines():
+			lines.append(line)
+		f.close()
+		return lines
+
 	def get_data(self):
+		path = self.args["path"]
+		self.lines = self._read_file(path)
 		return 0.0
--- data_logger/data_input/temperature_test.py
+++ data_logger/data_input/temperature_test.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 
 import temperature
 
@@ -6,7 +7,28 @@ class TemperatureTestCase(unittest.TestCase):
 
 	def setUp(self):
 		self.expectedName = "Room 42"
-		self.t = temperature.Temperature(self.expectedName)
+		self.expectedFile = "test-sensor-file.txt"
+		self.expectedArgs = {"path": self.expectedFile}
+		self.expectedLines = []
+		self.expectedLines.append("81 01 4b 01 7f ff 0c 10 9c : crc=9c YES\n")
+		self.expectedLines.append("81 01 4b 01 7f ff 0c 10 9c t=24062\n")
+
+		self._init_fake_sensor_file(self.expectedFile, self.expectedLines)
+
+		self.t = temperature.Temperature(self.expectedName, self.expectedArgs)
+
+	def tearDown(self):
+		os.remove(self.expectedFile)
+
+	def _init_fake_sensor_file(self, path, lines):
+		f = open(path, "w")
+		for line in lines:
+			f.write(line)
+		f.close()
 
 	def testGetData_returnsFloat(self):
 		self.assertEqual(True, type(self.t.get_data()) is float)
+
+	def testGetData_readsLinesFromFileSpecifiedAsPath(self):
+		self.t.get_data()
+		self.assertEqual(self.expectedLines, self.t.lines)
